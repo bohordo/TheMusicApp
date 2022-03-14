16fef001d6239753b174fed65076c1c17b717a31
bohordo <dabo16@gmail.com>
2022-03-14 10:15:03 -0500

    Adding the initial Spring Boot Project

---
+++ src/main/java/com/training/TheMusicApp/TheMusicAppApplication.java
@@ -0,0 +1,13 @@
+package com.training.TheMusicApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMusicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMusicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/TheMusicApp/TheMusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.TheMusicApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMusicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
