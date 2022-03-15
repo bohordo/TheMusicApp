855dc11ae26bea13434d4b155a2c3e25dbecd53b
bohordo <dabo16@gmail.com>
2022-03-15 13:34:36 -0500

    Fixed initialization of data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into song (id,name,artists,genre) values(UUID(),'bohordos song', 'bohordo', 'boho-metal');
+insert into user (id,username) values(UUID(),'bohordo');
--- src/main/resources/db/h2/initial-creation.sql
+++
@@ -1,4 +0,0 @@
-create table song (id binary(16) not null, name varchar(255), artists varchar(255), genre varchar(255), primary key (id));
-insert into song (id,name,artists,genre) values(UUID(),'bohordos song', 'bohordo', 'boho-metal');
-create table user (id binary(16) not null, username varchar(255), primary key (id));
-insert into user (id,username) values(UUID(),'bohordo');
