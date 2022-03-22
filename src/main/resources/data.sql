474be6216ecf93ec69aa60b8866c52f66ff83a79
bohordo <dabo16@gmail.com>
2022-03-22 18:50:35 -0500

    removed h2 dependencies and updated of data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into song (id,name,artists,genre) values(UUID(),'bohordos song', 'bohordo', 'boho-metal');
+insert into song (id,name,artists,number_of_likes) values(UUID(),'bohordos song', 'bohordo',0);
 insert into user (id,username) values(UUID(),'bohordo');
