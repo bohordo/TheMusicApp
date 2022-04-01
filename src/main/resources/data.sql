07feaffc5aad51ff96b6bd67ea3af1f6af84cc8f
bohordo <dabo16@gmail.com>
2022-04-01 15:09:55 -0500

    fixed some test issues related to the build

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into song (id,name,artists,number_of_likes) values(UUID(),'bohordos song', 'bohordo',0);
-insert into user (id,username) values(UUID(),'bohordo');
--- src/test/java/com/training/themusicapp/TheMusicAppApplicationTests.java
+++ src/test/java/com/training/themusicapp/TheMusicAppApplicationTests.java
@@ -9,5 +9,4 @@ class TheMusicAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
