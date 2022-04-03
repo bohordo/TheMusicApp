860f79a83ee64b73ca39e84f9d624455e5dde342
bohordo <dabo16@gmail.com>
2022-04-03 15:26:55 -0500

    Added unit test for the rest of the repository and entites

--- src/test/java/com/training/themusicapp/repository/UserRepositoryTest.java
+++ src/test/java/com/training/themusicapp/repository/UserRepositoryTest.java
@@ -1,10 +1,43 @@
 package com.training.themusicapp.repository;
 
+import com.training.themusicapp.repository.entity.UserEntity;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    List<UserEntity> userEntitiesList;
+
+    @Before
+    public void setUp(){
+        userEntitiesList = new ArrayList<>();
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId("1");
+        userEntity.setUsername("user-name");
+        userEntity.setTotalNumberOfLikes(1);
+        userEntitiesList.add(userEntity);
+        userRepository.saveAll(userEntitiesList);
+    }
+
+    @Test
+    public void userRepositoryTest(){
+        List<UserEntity> userEntity = (List<UserEntity>) userRepository.findAll();
+        assertEquals("1",userEntity.get(0).getId());
+        assertEquals("user-name",userEntity.get(0).getUsername());
+        assertEquals(1,userEntity.get(0).getTotalNumberOfLikes());
+    }
 }
--- src/test/java/com/training/themusicapp/repository/UserSongRepositoryTest.java
+++ src/test/java/com/training/themusicapp/repository/UserSongRepositoryTest.java
@@ -1,10 +1,43 @@
 package com.training.themusicapp.repository;
 
+import com.training.themusicapp.repository.entity.UserSongEntity;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserSongRepositoryTest {
+
+    @Autowired
+    UserSongRepository userSongRepository;
+
+    List<UserSongEntity> userSongEntitiesList;
+
+    @Before
+    public void setUp(){
+        userSongEntitiesList = new ArrayList<>();
+        UserSongEntity userSongEntity = new UserSongEntity();
+        userSongEntity.setId("11");
+        userSongEntity.setUserId("1");
+        userSongEntity.setSongId("1");
+        userSongEntitiesList.add(userSongEntity);
+        userSongRepository.saveAll(userSongEntitiesList);
+    }
+
+    @Test
+    public void userRepositoryTest(){
+        List<UserSongEntity> userSongEntities = (List<UserSongEntity>) userSongRepository.findAll();
+        assertEquals("11",userSongEntities.get(0).getId());
+        assertEquals("1",userSongEntities.get(0).getSongId());
+        assertEquals("1",userSongEntities.get(0).getSongId());
+    }
 }
