890854e88d949480f268fdefeab5bb71ee9d2caa
bohordo <dabo16@gmail.com>
2022-04-07 10:23:49 -0500

    adding some test

--- src/test/java/com/training/themusicapp/TheMusicAppApplicationTests.java
+++ src/test/java/com/training/themusicapp/TheMusicAppApplicationTests.java
@@ -3,10 +3,13 @@ package com.training.themusicapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertTrue;
+
 @SpringBootTest
 class TheMusicAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 }
--- src/test/java/com/training/themusicapp/mapper/MappingUtilTest.java
+++ src/test/java/com/training/themusicapp/mapper/MappingUtilTest.java
@@ -1,4 +1,15 @@
 package com.training.themusicapp.mapper;
 
+import org.junit.Test;
+import org.modelmapper.ModelMapper;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class MappingUtilTest {
+
+    @Test
+    public void mappingUtilTest(){
+        MappingUtil mappingUtil = new MappingUtil(new ModelMapper());
+        assertNotNull(mappingUtil);
+    }
 }
