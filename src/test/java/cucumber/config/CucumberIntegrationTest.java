77d7253ffef28ef44d097c5ea030c411bbe211d0
bohordo <dabo16@gmail.com>
2022-04-08 08:24:02 -0500

    cucumber files config and integration in the repo

--- src/test/java/cucumber/config/CucumberIntegrationTest.java
+++ src/test/java/cucumber/config/CucumberIntegrationTest.java
@@ -2,10 +2,27 @@ package cucumber.config;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"classpath:features"},
                  glue = {"classpath:cucumber/steps" })
+
+/*
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("bdd")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "usage")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "html:target/cucumber-reports")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME,
+        value = "myproject.cucumber.glue")*/
+//@CucumberContextConfiguration
+//@SpringBootTest(classes = SpringIntegrationTest.class)
 public class CucumberIntegrationTest {
 }
--- src/test/java/cucumber/config/SpringIntegrationTest.java
+++ src/test/java/cucumber/config/SpringIntegrationTest.java
@@ -1,13 +1,33 @@
 package cucumber.config;
 
+import com.training.themusicapp.repository.ArtistRepository;
+import com.training.themusicapp.repository.SongRepository;
+import com.training.themusicapp.repository.UserRepository;
+import com.training.themusicapp.repository.UserSongRepository;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import io.cucumber.spring.CucumberContextConfiguration;
 
+/*
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("cucumber")
 @ContextConfiguration(loader = SpringBootContextLoader.class)
+*/
 
+@CucumberContextConfiguration
+@SpringBootTest
+@ActiveProfiles("test")
 public class SpringIntegrationTest {
+
+    @MockBean
+    private SongRepository songRepository;
+    @MockBean
+    private ArtistRepository artistRepository;
+    @MockBean
+    private UserRepository userRepository;
+    @MockBean
+    private UserSongRepository userSongRepository;
 }
